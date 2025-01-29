db7f242f0abac446e415cf1d7bc6268e67eec4cd
Matthew Keathley <mrkeathley85@gmail.com>
2025-01-29 01:04:35 -0500

    Added attack animation demo. Added signage demo.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,22 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
+public enum PlayerState {
+	walk,
+	attack,
+	interact
+}
+
 public class PlayerMovement : MonoBehaviour {
 
+	public PlayerState currentState;
 	public float speed;
 	private Rigidbody2D myRigidbody;
 	private Vector3 change;
 	private Animator animator;
 
 	void Start() {
+		currentState = PlayerState.walk;
 		animator = GetComponent<Animator>();
 		myRigidbody = GetComponent<Rigidbody2D>();
 	}
@@ -19,17 +27,30 @@ public class PlayerMovement : MonoBehaviour {
 		change.x = Input.GetAxisRaw("Horizontal");
 		change.y = Input.GetAxisRaw("Vertical");
 
-		if (change != Vector3.zero) {
+		if(Input.GetButtonDown("attack") && currentState != PlayerState.attack) {
+			StartCoroutine(AttackCo());
+		} else if (currentState == PlayerState.walk) {
+			if (change != Vector3.zero) {
 			animator.SetFloat("moveX", change.x);
 			animator.SetFloat("moveY", change.y);
 			animator.SetBool("moving", true);
-		} else {
-			animator.SetBool("moving", false);
+			} else {
+				animator.SetBool("moving", false);
+			}
 		}
 	}
 
 	void FixedUpdate() {
 		MoveCharacter();
+	}
+
+	private IEnumerator AttackCo() {
+		animator.SetBool("attacking", true);
+		currentState = PlayerState.attack;
+		yield return null;
+		animator.SetBool("attacking", false);
+		yield return new WaitForSeconds(.33f);
+		currentState = PlayerState.walk;
 	}
 
 	void MoveCharacter() {
---
+++ Assets/Scripts/Sign.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Sign : MonoBehaviour {
+
+    public GameObject dialogBox;
+    public Text dialogText;
+    public string dialog;
+    public bool playerInRange;
+
+    void Start() {}
+
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.Space) && playerInRange) {
+            if (dialogBox.activeInHierarchy) {
+                dialogBox.SetActive(false);
+            } else {
+                dialogText.text = dialog;
+                dialogBox.SetActive(true);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            playerInRange = false;
+            dialogBox.SetActive(false);
+        }
+    }
+}
