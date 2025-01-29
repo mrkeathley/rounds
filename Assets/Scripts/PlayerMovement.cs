20820911440e702838948cc23d948481847fb34d
Matthew Keathley <mrkeathley85@gmail.com>
2025-01-28 21:09:07 -0500

    Added sample player with movement and animations.

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D myRigidbody;
+	private Vector3 change;
+	private Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+		myRigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		change = Vector3.zero;
+		change.x = Input.GetAxisRaw("Horizontal");
+		change.y = Input.GetAxisRaw("Vertical");
+		if (change != Vector3.zero) {
+			MoveCharacter();
+			animator.SetFloat("moveX", change.x);
+			animator.SetFloat("moveY", change.y);
+			animator.SetBool("moving", true);
+		} else {
+			animator.SetBool("moving", false);
+		}
+	}
+
+	void MoveCharacter() {
+		myRigidbody.MovePosition(
+			transform.position + change * speed * Time.deltaTime
+		);		
+	}
+    
+}
